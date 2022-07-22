51ae0b0f5527d42da7cc13ad7e7aa3c636957265
ultram4rine <ultramarine730@gmail.com>
2022-07-22 22:31:43 +0400

    make my changes

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
 
+try {
+  const changelog = core.getInput("changelog");
+  const version = core.getInput("version");
+  const output = core.getInput("output");
+  console.log(`Proceed ${changelog} at ${version}`);
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+  const rl = readline.createInterface({
+    input: fs.createReadStream(path.resolve(changelog)),
+  });
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+  let changes = "";
+  let startRecord = false;
+  rl.on("line", (line) => {
+    if (line.startsWith("## [")) {
+      if (line.startsWith(`## [${version}]`)) {
+        startRecord = true;
+      } else if (startRecord) {
+        // Stop because we found next version header.
+        rl.close();
+      }
+    } else {
+      if (startRecord) {
+        changes = changes.concat(line);
+      }
+    }
+  });
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+  core.setOutput("changes", changes);
+
+  if (output) {
+    fs.writeFileSync(output, changes, { flag: "a" });
   }
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-run();

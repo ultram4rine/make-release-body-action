0cf61a37aeeb77bf5769915b4acd6fa9d9eb5749
ultram4rine <ultramarine730@gmail.com>
2023-01-21 01:54:53 +0400

    refactor

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test("test runs", () => {
-  process.env["INPUT_CHANGELOG"] = "test/changelog";
+  process.env["INPUT_CHANGELOG"] = "./tests/changelog";
   process.env["INPUT_VERSION"] = "1.0.0";
-  process.env["INPUT_OUTPUT"] = "test/changes.txt";
-  const ip = path.join(__dirname, "index.js");
+  process.env["INPUT_OUTPUT"] = "./tests/changes.txt";
+  const ip = path.join(__dirname, "../lib/index.js");
   const result = cp.execSync(`node ${ip}`, { env: process.env }).toString();
   console.log(result);
 });

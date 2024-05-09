8e58c2efaca11e05e5e90ab020efd1d9ec3b03fd
ultram4rine <ultramarine730@gmail.com>
2024-05-09 23:10:05 +0400

    use latest repo template, update node to v20

---
+++ __tests__/main.test.js
@@ -0,0 +1,92 @@
+/**
+ * Unit tests for the action's main functionality, src/main.js
+ */
+const fs = require('fs')
+const core = require('@actions/core')
+const main = require('../src/main')
+
+// Mock the GitHub Actions core library
+const debugMock = jest.spyOn(core, 'debug').mockImplementation()
+const getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+const setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
+const setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+
+// Mock the action's main function
+const runMock = jest.spyOn(main, 'run')
+
+describe('action', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('sets the time output', async () => {
+    // Set the action's inputs as return values from core.getInput()
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'changelog':
+          return './__tests__/changelog'
+        case 'version':
+          return '1.0.0'
+        case 'output':
+          return './__tests__/changes.txt'
+        default:
+          return ''
+      }
+    })
+
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    // Verify that all of the core library functions were called correctly
+    expect(debugMock).toHaveBeenNthCalledWith(1, 'Found 1.0.0 section')
+    expect(setOutputMock).toHaveBeenNthCalledWith(
+      1,
+      'changes',
+      fs.readFileSync('./__tests__/changes.txt').toString()
+    )
+  })
+
+  it('sets a failed status', async () => {
+    // Set the action's inputs as return values from core.getInput()
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'changelog':
+          return 'not-valid-changelog.txt'
+        default:
+          return ''
+      }
+    })
+
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    // Verify that all of the core library functions were called correctly
+    expect(setFailedMock).toHaveBeenNthCalledWith(
+      1,
+      expect.stringContaining('ENOENT: no such file or directory')
+    )
+  })
+
+  it('fails if no input is provided', async () => {
+    // Set the action's inputs as return values from core.getInput()
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'changelog':
+          throw new Error('Input required and not supplied: changelog')
+        case 'version':
+          throw new Error('Input required and not supplied: version')
+        default:
+          return ''
+      }
+    })
+
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    // Verify that all of the core library functions were called correctly
+    expect(setFailedMock).toHaveBeenNthCalledWith(
+      1,
+      'Input required and not supplied: changelog'
+    )
+  })
+})
